6ac66fd22931d452df39e1a854b14aadc5e3e0da
jakub.sztachelski <jakub.sztachelski@allegro.pl>
2021-05-26 14:06:32 +0200

    add gcs fuse utility

--- bash_commands.sh
+++ bash_commands.sh
@@ -1,11 +1,11 @@
 # in order to execute this file type in terminal:
 # wget -O - https://raw.githubusercontent.com/sztach/configs/master/bash_commands.sh | bash
 
+cd ~
 
 # install tmux and download tmux configuration file
 apt-get install -y tmux
 curl -fLo "./.tmux.conf" https://raw.githubusercontent.com/sztach/configs/master/.tmux.conf
-mv .tmux.conf ~/.tmux.conf
 
 # install neo vim
 # curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
@@ -22,11 +22,20 @@ mv .tmux.conf ~/.tmux.conf
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-# download .vimrc file and install desired plugins
-curl -fLo "./.vimrc" https://raw.githubusercontent.com/sztach/configs/master/.vimrc
-mv .vimrc ~/.vimrc
-vim -c "PlugInstall"
-
 # download .bashrc addons
 wget -O - https://raw.githubusercontent.com/sztach/configs/master/bashrc_addons >> ~/.bashrc
 
+# install gcs fuse and mount desired bucket to vm instance
+export GCSFUSE_REPO=gcsfuse-`lsb_release -c -s`
+echo "deb http://packages.cloud.google.com/apt $GCSFUSE_REPO main" | sudo tee /etc/apt/sources.list.d/gcsfuse.list
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install gcsfuse
+mkdir bucket
+gcsfuse js_advengers ./bucket
+
+# download .vimrc file and install desired plugins
+curl -fLo "./.vimrc" https://raw.githubusercontent.com/sztach/configs/master/.vimrc
+vim -c "PlugInstall"
+
+exit
