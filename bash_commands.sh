3d6f226a2d97023727319192de2293889b254ad8
jakub.sztachelski <jakub.sztachelski@allegro.pl>
2021-05-27 11:49:45 +0200

    add oh my bash to conf

--- bash_commands.sh
+++ bash_commands.sh
@@ -22,7 +22,8 @@ curl -fLo "./.tmux.conf" https://raw.githubusercontent.com/sztach/configs/master
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-# download .bashrc addons
+# download .bashrc addons and oh my bash 
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
 wget -O - https://raw.githubusercontent.com/sztach/configs/master/bashrc_addons >> ~/.bashrc
 
 # install gcs fuse and mount desired bucket to vm instance
