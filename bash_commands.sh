16521db76d9743c290918e655244854af911f582
jakub.sztachelski <jakub.sztachelski@allegro.pl>
2021-08-03 15:54:24 +0200

    add .ideavimrc and shell script for updating bashrc and vimrc files

--- bash_commands.sh
+++ bash_commands.sh
@@ -13,7 +13,6 @@ curl -fLo "./.tmux.conf" https://raw.githubusercontent.com/sztach/configs/master
 # ./nvim.appimage --appimage-extract
 # ./squashfs-root/AppRun --version
 # mv squashfs-root / && ln -s /squashfs-root/AppRun /usr/bin/nvim
-
 # vim-plug for neo vim
 # sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
 #        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
@@ -33,7 +32,6 @@ curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 sudo apt-get update
 sudo apt-get install gcsfuse
 mkdir bucket
-gcsfuse cvm_boosters ./bucket
 
 # install linter and formatter
 sudo apt-get install -y black
